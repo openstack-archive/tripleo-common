6ca55cc4a4b21a4c1d24872d2da877f006e16881
Jan Provaznik <jprovazn@redhat.com>
2015-05-19 11:05:10 +0200

    Scale down heat stack
    Allows scaling down heat stack deployed using tuskar templates.
    It expects list if nova instance ids on input, for each instance
    it finds heat resource and tuskar role name and then it updates
    tuskar plan with updateed number of nodes for each role and
    it updates heat stack passing it list of resource ids representing
    instances to remove.
    
    Change-Id: Ib78b12ea45792d607a354b444c6c05534a11437d

--- tripleo_common/scale.py
+++ tripleo_common/scale.py
@@ -40,8 +40,62 @@ class ScaleManager(object):
             raise ValueError("Role %s has already %s nodes, can't set lower "
                              "value" % (role, param['value']))
         self.plan = self.tuskarclient.plans.patch(
-            self.plan.uuid, [{'name': param_name,
-                              'value': str(num)}])
+            self.plan.uuid,
+            [{'name': param_name, 'value': str(num)}])
+        self._update_stack()
+
+    def scaledown(self, instances):
+        resources = self.heatclient.resources.list(self.stack_id,
+                                                   nested_depth=5)
+        resources_by_role = {}
+        instance_list = list(instances)
+        for res in resources:
+            try:
+                instance_list.remove(res.physical_resource_id)
+            except ValueError:
+                continue
+
+            stack_name, stack_id = next(
+                x['href'] for x in res.links if
+                x['rel'] == 'stack').rsplit('/', 2)[1:]
+            # get resource to remove from resource group (it's parent resource
+            # of nova server)
+            role_resource = next(x for x in resources if
+                                 x.physical_resource_id == stack_id)
+            # get tuskar role name from resource_type,
+            # resource_type is in format like "Tuskar::Compute-1"
+            role = role_resource.resource_type.rsplit('::', 1)[-1]
+            if role not in resources_by_role:
+                resources_by_role[role] = []
+            resources_by_role[role].append(role_resource)
+
+        if instance_list:
+            raise ValueError(
+                "Couldn't find following instances in stack %s: %s" %
+                (self.stack_id, ','.join(instance_list)))
+
+        # decrease count for each tuskar role in tuskar plan and add removal
+        # policy for each resource group
+        patch_params = []
+        stack_params = {}
+        for role, role_resources in resources_by_role.items():
+            param_name = "{0}::count".format(role)
+            old_count = next(x['value'] for x in self.plan.parameters if
+                             x['name'] == param_name)
+            count = max(int(old_count) - len(role_resources), 0)
+            patch_params.append({'name': param_name, 'value': str(count)})
+            # add instance resource names into removal_policies
+            # so heat knows which instances should be removed
+            removal_param = "{0}::removal_policies".format(role)
+            stack_params[removal_param] = [{
+                'resource_list': [r.resource_name for r in role_resources]
+            }]
+
+        LOG.debug('updating plan %s: %s', self.plan.uuid, patch_params)
+        self.plan = self.tuskarclient.plans.patch(self.plan.uuid, patch_params)
+        self._update_stack(parameters=stack_params)
+
+    def _update_stack(self, parameters={}):
         tpl_dir = libutils.save_templates(
             self.tuskarclient.plans.templates(self.plan.uuid))
         try:
@@ -55,7 +109,8 @@ class ScaleManager(object):
                 'template': template,
                 'files': dict(list(tpl_files.items()) +
                               list(env_files.items())),
-                'environment': env
+                'environment': env,
+                'parameters': parameters
             }
 
             LOG.debug('stack update params: %s', fields)
--- tripleo_common/tests/test_scale.py
+++ tripleo_common/tests/test_scale.py
@@ -40,13 +40,34 @@ class ScaleManagerTest(base.TestCase):
     def setUp(self):
         super(ScaleManagerTest, self).setUp()
         self.image = collections.namedtuple('image', ['id'])
-        self.heatclient = mock.MagicMock()
         self.tuskarclient = mock.MagicMock()
         self.tuskarclient.plans.patch.return_value = mock_plan()
         self.tuskarclient.plans.templates.return_value = {
             'plan.yaml': 'template body',
             'environment.yaml': 'resource_registry: {}\n',
         }
+        self.heatclient = mock.MagicMock()
+        self.heatclient.resources.list.return_value = [
+            mock.MagicMock(
+                links=[{'rel': 'stack',
+                        'href': 'http://192.0.2.1:8004/v1/'
+                                'a959ac7d6a4a475daf2428df315c41ef/'
+                                'stacks/overcloud/123'}],
+                logical_resource_id='logical_id',
+                physical_resource_id='resource_id',
+                resource_type='compute-1'
+            ),
+            mock.MagicMock(
+                links=[{'rel': 'stack',
+                        'href': 'http://192.0.2.1:8004/v1/'
+                                'a959ac7d6a4a475daf2428df315c41ef/'
+                                'stacks/overcloud/124'}],
+                logical_resource_id='node0',
+                physical_resource_id='123',
+                resource_type='compute-1',
+                resource_name='node0',
+            )
+        ]
 
     @mock.patch('heatclient.common.template_utils.'
                 'process_multiple_environments_and_files')
@@ -67,18 +88,44 @@ class ScaleManagerTest(base.TestCase):
             '5', [{'name': 'compute-1::count', 'value': '3'}])
         self.heatclient.stacks.update.assert_called_once_with(
             stack_id='stack', template='template body', environment={},
-            files={})
+            files={}, parameters={})
 
-    @mock.patch('heatclient.common.template_utils.'
-                'process_multiple_environments_and_files')
-    @mock.patch('heatclient.common.template_utils.get_template_contents')
-    @mock.patch('tripleo_common.libutils.open', create=True)
     @mock.patch('tuskarclient.common.utils.find_resource')
-    def test_invalid_scaleup(self, mock_find_resource, mock_open,
-                             mock_template_contents, mock_env_files):
+    def test_invalid_scaleup(self, mock_find_resource):
         mock_find_resource.return_value = mock_plan()
         manager = scale.ScaleManager(tuskarclient=self.tuskarclient,
                                      heatclient=self.heatclient,
                                      stack_id='stack',
                                      plan_id='plan')
         self.assertRaises(ValueError, manager.scaleup, 'compute-1', 1)
+
+    @mock.patch('heatclient.common.template_utils.'
+                'process_multiple_environments_and_files')
+    @mock.patch('heatclient.common.template_utils.get_template_contents')
+    @mock.patch('tripleo_common.libutils.open', create=True)
+    @mock.patch('tuskarclient.common.utils.find_resource')
+    def test_scaledown(self, mock_find_resource, mock_open,
+                       mock_template_contents, mock_env_files):
+        mock_find_resource.return_value = mock_plan()
+        mock_template_contents.return_value = ({}, 'template body')
+        mock_env_files.return_value = ({}, {})
+        manager = scale.ScaleManager(tuskarclient=self.tuskarclient,
+                                     heatclient=self.heatclient,
+                                     stack_id='stack',
+                                     plan_id='plan')
+        manager.scaledown(['resource_id'])
+        self.tuskarclient.plans.patch.assert_called_once_with(
+            '5', [{'name': 'compute-1::count', 'value': '1'}])
+        self.heatclient.stacks.update.assert_called_once_with(
+            stack_id='stack', template='template body', environment={},
+            files={}, parameters={'compute-1::removal_policies': [
+                {'resource_list': ['node0']}]})
+
+    @mock.patch('tuskarclient.common.utils.find_resource')
+    def test_invalid_scaledown(self, mock_find_resource):
+        mock_find_resource.return_value = mock_plan()
+        manager = scale.ScaleManager(tuskarclient=self.tuskarclient,
+                                     heatclient=self.heatclient,
+                                     stack_id='stack',
+                                     plan_id='plan')
+        self.assertRaises(ValueError, manager.scaledown, 'invalid_resource_id')
