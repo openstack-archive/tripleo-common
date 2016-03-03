4685a0b5bd324ff12e0c237f735426511fb9680e
Ben Nemec <bnemec@redhat.com>
2016-03-03 19:45:32 +0000

    Add capabilities filter for Nova
    It turns out that the ComputeCapabilitiesFilter built-in to Nova
    doesn't respect capabilities passed in scheduler_hints, so we can't
    use it for predictable placement.  Adding this filter to the
    undercloud Nova filter list will allow us to do so.
    
    Instead of pulling in all of Nova as a test requirement, I've added
    a fake_nova module to the source tree, which is injected as 'nova'
    when unit tests are being run.  A check is included to make sure
    nova isn't being imported for real, as well as a README explaining
    the reasoning behind the fake_nova module.
    
    Change-Id: I0618a3b9e3c33af7cdc78db4b6994d463b8aeda9
    (cherry picked from commit f1c6ac9f2eb05064fca1cb7a92148b96068ed251)

---
+++ tripleo_common/filters/capabilities_filter.py
@@ -0,0 +1,35 @@
+# Copyright 2016 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nova.scheduler import filters
+
+
+class TripleOCapabilitiesFilter(filters.BaseHostFilter):
+    """Filter hosts based on capabilities in boot request
+
+    The standard Nova ComputeCapabilitiesFilter does not respect capabilities
+    requested in the scheduler_hints field, so we need a custom one in order
+    to be able to do predictable placement of nodes.
+    """
+
+    # list of hosts doesn't change within a request
+    run_filter_once_per_request = True
+
+    def host_passes(self, host_state, spec_obj):
+        host_node = host_state.stats.get('node')
+        instance_node = spec_obj.scheduler_hints.get('capabilities:node')
+        # The instance didn't request a specific node
+        if not instance_node:
+            return True
+        return host_node == instance_node[0]
