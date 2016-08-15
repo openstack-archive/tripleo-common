b82fc6f21245cba7fadb35a6676433f015aad516
Florian Fuchs <flfuchs@redhat.com>
2016-08-15 15:38:23 +0200

    Exclude more unneeded files from default plan
    This patch exludes more file types from the tarball uploaded to swift as
    the default deployment plan.
    
    Change-Id: I8b6d8de8d7662604cdb871fa6a4fb872c7937e25
    Closes-Bug: #1613286

--- tripleo_common/utils/tarball.py
+++ tripleo_common/utils/tarball.py
@@ -23,7 +23,8 @@ def create_tarball(directory, filename, options='-czf'):
     """Create a tarball of a directory."""
     LOG.debug('Creating tarball of %s at location %s' % (directory, filename))
     processutils.execute('/usr/bin/tar', '-C', directory, options, filename,
-                         '--exclude', '.git', '--exclude', '.tox', '.')
+                         '--exclude', '.git', '--exclude', '.tox',
+                         '--exclude', '*.pyc', '--exclude', '*.pyo', '.')
 
 
 def tarball_extract_to_swift_container(object_client, filename, container):
