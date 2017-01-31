bc99748ff0a001bd4872f55aaaeddd7b786b038e
Brad P. Crochet <brad@redhat.com>
2017-01-31 11:18:57 -0500

    Implement check boot images as a custom action
    Adds a custom action to check the status of the boot images in glance.
    
    Change-Id: I3e16989a788177c4fb5b102d280d212a2c8f754b
    Partial-Bug: #1638697

--- tripleo_common/constants.py
+++ tripleo_common/constants.py
@@ -112,3 +112,7 @@ DEFAULT_BAREMETAL_API_VERSION = '1.29'
 # The name of the file which holds the Mistral environment contents for plan
 # import/export
 PLAN_ENVIRONMENT = 'plan-environment.yaml'
+
+DEFAULT_DEPLOY_KERNEL_NAME = 'bm-deploy-kernel'
+
+DEFAULT_DEPLOY_RAMDISK_NAME = 'bm-deploy-ramdisk'
