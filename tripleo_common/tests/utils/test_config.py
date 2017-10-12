11e8ac5148f1ad0436f33cd9ee24077280158e2c
James Slagle <jslagle@redhat.com>
2017-10-12 11:28:20 -0400

    Additional tests for config-download
    Adds some unit and functional tests for the functionality added in
    I7d7f6b831b8566390d8f747fb6f45e879b0392ba
    
    implements: blueprint ansible-config-download
    Change-Id: I4ca3c801388c301dfd4dcce7b0e98814692f6a39

--- tripleo_common/tests/utils/test_config.py
+++ tripleo_common/tests/utils/test_config.py
@@ -10,13 +10,16 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
+import datetime
 import fixtures
 import mock
 import os
+import yaml
 
 from mock import call
 from mock import patch
 
+from tripleo_common import constants
 from tripleo_common.tests import base
 from tripleo_common.tests.fake_config import fakes
 from tripleo_common.utils import config as ooo_config
@@ -148,3 +151,138 @@ class TestConfig(base.TestCase):
                 ['upgrade_tasks'], role, filepath)
             self.assertTrue(os.path.isfile(filepath))
             self.assertEqual(expected_tasks[role], playbook_tasks)
+
+    def test_get_server_data(self):
+        heat = mock.MagicMock()
+        self.config = ooo_config.Config(heat)
+        stack = 'overcloud'
+        role_names = ['Controller', 'Compute', 'Custom']
+        heat.resources.list.return_value = ['fakeserver']
+        server_data = self.config.get_server_data(stack, role_names)
+        self.assertEqual(heat.resources.list.call_count, 3)
+        self.assertEqual(
+            heat.resources.list.call_args_list[0],
+            mock.call(stack,
+                      filters=dict(type='OS::TripleO::ControllerServer'),
+                      nested_depth=constants.NESTED_DEPTH,
+                      with_detail=True))
+        self.assertEqual(
+            heat.resources.list.call_args_list[1],
+            mock.call(stack,
+                      filters=dict(type='OS::TripleO::ComputeServer'),
+                      nested_depth=constants.NESTED_DEPTH,
+                      with_detail=True))
+        self.assertEqual(
+            heat.resources.list.call_args_list[2],
+            mock.call(stack,
+                      filters=dict(type='OS::TripleO::CustomServer'),
+                      nested_depth=constants.NESTED_DEPTH,
+                      with_detail=True))
+        self.assertEqual(server_data,
+                         ['fakeserver', 'fakeserver', 'fakeserver'])
+
+    def test_get_deployment_data(self):
+        heat = mock.MagicMock()
+        self.config = ooo_config.Config(heat)
+        stack = 'overcloud'
+        first = mock.MagicMock()
+        first.creation_time = datetime.datetime.now() - datetime.timedelta(2)
+        second = mock.MagicMock()
+        second.creation_time = datetime.datetime.now() - datetime.timedelta(1)
+        third = mock.MagicMock()
+        third.creation_time = datetime.datetime.now()
+        # Set return_value in a nonsorted order, as we expect the function to
+        # sort, so that's what we want to test
+        heat.resources.list.return_value = [second, third, first]
+
+        deployment_data = self.config.get_deployment_data(stack)
+        self.assertTrue(heat.resources.list.called)
+        self.assertEqual(
+            heat.resources.list.call_args,
+            mock.call(stack,
+                      filters=dict(name=constants.TRIPLEO_DEPLOYMENT_RESOURCE),
+                      nested_depth=constants.NESTED_DEPTH,
+                      with_detail=True))
+        self.assertEqual(deployment_data,
+                         [first, second, third])
+
+    def _get_config_data(self, datafile):
+        config_data_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'data',
+            datafile)
+        config_data = yaml.safe_load(open(config_data_path).read())
+        server_data = []
+        deployment_data = []
+
+        for server in config_data['servers']:
+            server_mock = mock.MagicMock()
+            server_mock.physical_resource_id = server['physical_resource_id']
+            server_mock.name = server['name']
+            server_mock.attributes = {'OS::stack_id': server['OS::stack_id'],
+                                      'name': server['name']}
+            server_data.append(server_mock)
+
+        for deployment in config_data['deployments']:
+            deployment_mock = mock.MagicMock()
+            deployment_mock.id = deployment['deployment']
+            deployment_mock.attributes = dict(
+                value=dict(server=deployment['server'],
+                           deployment=deployment['deployment'],
+                           config=deployment['config'],
+                           name=deployment['name']))
+            deployment_data.append(deployment_mock)
+
+        server_id_data = config_data['server_id_data']
+        configs = config_data['configs']
+
+        return server_data, server_id_data, deployment_data, configs
+
+    def _get_config_dict(self, deployment):
+        config = self.configs[deployment.attributes['value']['config']].copy()
+        config['inputs'] = []
+        config['inputs'].append(dict(
+            name='deploy_server_id',
+            value=deployment.attributes['value']['server']))
+        return config
+
+    def _get_yaml_file(self, file_name):
+        file_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'data',
+            file_name)
+        return yaml.safe_load(open(file_path).read())
+
+    @patch('tripleo_common.utils.config.Config.get_config_dict')
+    @patch('tripleo_common.utils.config.Config.get_server_id_data')
+    @patch('tripleo_common.utils.config.Config.get_deployment_data')
+    @patch('tripleo_common.utils.config.Config.get_server_data')
+    def test_config_download(self, mock_server_data, mock_deployment_data,
+                             mock_server_id_data, mock_config_dict):
+        heat = mock.MagicMock()
+        self.config = ooo_config.Config(heat)
+        stack = 'overcloud'
+
+        server_data, server_id_data, deployment_data, configs = \
+            self._get_config_data('config_data.yaml')
+        self.configs = configs
+
+        mock_server_data.return_value = server_data
+        mock_server_id_data.return_value = server_id_data
+        mock_deployment_data.return_value = deployment_data
+        mock_config_dict.side_effect = self._get_config_dict
+
+        tmp_path = self.config.download_config(stack, '/tmp')
+        print("config at %s" % tmp_path)
+
+        for f in ['overcloud-controller-0',
+                  'overcloud-novacompute-0',
+                  'overcloud-novacompute-1',
+                  'overcloud-novacompute-2',
+                  'Controller',
+                  'Compute', ]:
+
+            self.assertEqual(
+                yaml.safe_load(
+                    open(os.path.join(tmp_path, 'group_vars', f)).read()),
+                self._get_yaml_file(f))
