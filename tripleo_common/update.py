b9105a6534f8523bed029cb2919cbc63214378cf
Thomas Herve <therve@redhat.com>
2018-01-22 15:37:47 +0100

    Remove PackageUpdateManager
    This is unused now that we use ansible to trigger updates.
    
    Change-Id: I8a2cdac9070fded34683b85858e1fdcd95dfea6d

--- tripleo_common/update.py
+++ tripleo_common/update.py
@@ -13,15 +13,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import logging
-import time
-
 from heatclient.common import template_utils
 
-from tripleo_common import _stack_update
 from tripleo_common import constants
-
-LOG = logging.getLogger(__name__)
 
 
 def add_breakpoints_cleanup_into_env(env):
@@ -31,57 +25,3 @@ def add_breakpoints_cleanup_into_env(env):
                 constants.UPDATE_RESOURCE_NAME: {'hooks': []}}}}
         }
     })
-
-
-class PackageUpdateManager(_stack_update.StackUpdateManager):
-    def __init__(self, heatclient, novaclient, stack_id, stack_fields):
-        stack = heatclient.stacks.get(stack_id)
-        self.stack_fields = stack_fields
-        super(PackageUpdateManager, self).__init__(
-            heatclient=heatclient, novaclient=novaclient, stack=stack,
-            hook_type='pre-update', nested_depth=5,
-            hook_resource=constants.UPDATE_RESOURCE_NAME)
-
-    def update(self, timeout_mins=constants.STACK_TIMEOUT_DEFAULT):
-        env = {}
-        if 'environment' in self.stack_fields:
-            env = self.stack_fields['environment']
-
-        template_utils.deep_update(env, {
-            'resource_registry': {
-                'resources': {
-                    '*': {
-                        '*': {
-                            constants.UPDATE_RESOURCE_NAME: {
-                                'hooks': 'pre-update'}
-                        }
-                    }
-                }
-            }
-        })
-
-        # time rounded to seconds
-        timestamp = int(time.time())
-
-        stack_params = {
-            'DeployIdentifier': timestamp,
-            'UpdateIdentifier': timestamp,
-            'StackAction': 'UPDATE'
-        }
-        template_utils.deep_update(env, {'parameter_defaults': stack_params})
-
-        self.stack_fields['environment'] = env
-
-        fields = {
-            'existing': True,
-            'stack_id': self.stack.id,
-            'template': self.stack_fields['template'],
-            'files': self.stack_fields['files'],
-            'environment': self.stack_fields['environment'],
-            'timeout_mins': timeout_mins,
-            'stack_name': self.stack_fields['stack_name'],
-        }
-
-        LOG.info('updating stack: %s', self.stack.stack_name)
-        LOG.debug('stack update params: %s', fields)
-        self.heatclient.stacks.update(**fields)
