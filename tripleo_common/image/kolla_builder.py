74f70b7a33fe7589cdb28ab44235f83aa5cda158
Giulio Fidente <gfidente@redhat.com>
2018-03-16 15:53:44 +0100

    Use new ceph/daemon tags
    The newer naming scheme replaces 'tag' with an actual version
    number and appends the target architecture to the tag name.
    
    Ultimately it allows us to pin to 'known to work' builds of the
    images.
    
    Change-Id: If080fa40f674cb9254f2b411c59f88ac54c74d12

--- tripleo_common/image/kolla_builder.py
+++ tripleo_common/image/kolla_builder.py
@@ -31,7 +31,7 @@ CONTAINER_IMAGE_PREPARE_PARAM = [{
         'namespace': 'docker.io/tripleomaster',
         'ceph_namespace': 'docker.io/ceph',
         'ceph_image': 'daemon',
-        'ceph_tag': 'tag-stable-3.0-luminous-centos-7',
+        'ceph_tag': 'v3.0.1-stable-3.0-luminous-centos-7-x86_64',
         'name_prefix': 'centos-binary-',
         'name_suffix': '',
         'tag': 'current-tripleo',
--- tripleo_common/tests/image/test_kolla_builder.py
+++ tripleo_common/tests/image/test_kolla_builder.py
@@ -167,7 +167,7 @@ class TestKollaImageBuilderTemplate(base.TestCase):
                 'namespace': 'docker.io/tripleomaster',
                 'ceph_namespace': 'docker.io/ceph',
                 'ceph_image': 'daemon',
-                'ceph_tag': 'tag-stable-3.0-luminous-centos-7',
+                'ceph_tag': 'v3.0.1-stable-3.0-luminous-centos-7-x86_64',
                 'name_prefix': 'centos-binary-',
                 'name_suffix': '',
                 'tag': 'current-tripleo',
