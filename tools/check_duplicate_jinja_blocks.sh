79a1a4b275cd7de126b4d7c5e3d1f8ae6ec6bfe0
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-09-17 11:11:50 +0200

    Add bashate in gate jobs and fix existing errors
    Change-Id: I3cbf754ea9c037df8b95eb6f83dbe8ce09bfd717
    Signed-off-by: Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>

--- tools/check_duplicate_jinja_blocks.sh
+++ tools/check_duplicate_jinja_blocks.sh
@@ -7,15 +7,15 @@ grep '{% block' $path |
 sort |
 uniq -c |
 awk -v myname=$myname '
-	$1 == 2 {
-		printf "%s: found duplicate block for %s\n", myname, $4
-		retval=1
-	}
+    $1 == 2 {
+        printf "%s: found duplicate block for %s\n", myname, $4
+        retval=1
+    }
 
-	END {exit retval}
+    END {exit retval}
 '
 
 if [ $? -ne 0 ]; then
-	echo "$myname: duplicate jinja block declarations found in $path" >&2
-	exit 1
+    echo "$myname: duplicate jinja block declarations found in $path" >&2
+    exit 1
 fi
