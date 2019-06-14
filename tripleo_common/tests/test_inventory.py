f08c6058e3db6fe07c73fcf625d1b51945bf4f5a
Gael Chamoulaud <gchamoul@redhat.com>
2019-06-14 14:27:12 +0200

    Update the Undercloud Services list in the inventory.
    Since Stein, the name of the OpenStack services on the Undercloud have
    been renamed as 'tripleo_*.service'. This patch makes the Undercloud
    service names evolve accordingly.
    
    Change-Id: I7c290f908fd685b801f49ba80531c14ce54677bd
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>
    (cherry picked from commit 6942ac007f66fdf22a1a7678da608b28eebc6b4e)

--- tripleo_common/tests/test_inventory.py
+++ tripleo_common/tests/test_inventory.py
@@ -213,12 +213,12 @@ class TestInventory(base.TestCase):
                          'plan': 'overcloud',
                          'project_name': 'admin',
                          'undercloud_service_list': [
-                             'openstack-nova-compute',
-                             'openstack-heat-engine',
-                             'openstack-ironic-conductor',
-                             'openstack-swift-container',
-                             'openstack-swift-object',
-                             'openstack-mistral-engine'],
+                             'tripleo_nova_compute',
+                             'tripleo_heat_engine',
+                             'tripleo_ironic_conductor',
+                             'tripleo_swift_container_server',
+                             'tripleo_swift_object_server',
+                             'tripleo_mistral_engine'],
                          'undercloud_swift_url': 'anendpoint',
                          'username': 'admin'}}}
         inv_list = inventory.list()
@@ -297,12 +297,12 @@ class TestInventory(base.TestCase):
                          'plan': 'overcloud',
                          'project_name': 'admin',
                          'undercloud_service_list': [
-                             'openstack-nova-compute',
-                             'openstack-heat-engine',
-                             'openstack-ironic-conductor',
-                             'openstack-swift-container',
-                             'openstack-swift-object',
-                             'openstack-mistral-engine'],
+                             'tripleo_nova_compute',
+                             'tripleo_heat_engine',
+                             'tripleo_ironic_conductor',
+                             'tripleo_swift_container_server',
+                             'tripleo_swift_object_server',
+                             'tripleo_mistral_engine'],
                          'undercloud_swift_url':
                          'anendpoint' if session else None,
                          'username': 'admin'}}}
@@ -399,12 +399,12 @@ class TestInventory(base.TestCase):
                                     'plan': 'overcloud',
                                     'project_name': 'admin',
                                     'undercloud_service_list': [
-                                        'openstack-nova-compute',
-                                        'openstack-heat-engine',
-                                        'openstack-ironic-conductor',
-                                        'openstack-swift-container',
-                                        'openstack-swift-object',
-                                        'openstack-mistral-engine'],
+                                        'tripleo_nova_compute',
+                                        'tripleo_heat_engine',
+                                        'tripleo_ironic_conductor',
+                                        'tripleo_swift_container_server',
+                                        'tripleo_swift_object_server',
+                                        'tripleo_mistral_engine'],
                                     'undercloud_swift_url': 'anendpoint',
                                     'username': 'admin'}}}
         if extra_vars:
