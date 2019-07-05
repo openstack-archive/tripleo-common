7d0da3b49f02d8a95c642ddbae7c21a3f86a92b3
Rabi Mishra <ramishra@redhat.com>
2019-07-05 09:45:21 +0530

    Stop writing allnodes_vars in inventory
    In commit 4c34147cc1d268116421252d46a7204227f10fb3 AllNodesConfig
    stack output was removed, so there is no point of writing
    all_nodes_vars any more.
    
    Change-Id: If2656e6b5ef6a6ad8a18c85aadbfc982a4e646a5

--- tripleo_common/utils/config.py
+++ tripleo_common/utils/config.py
@@ -217,7 +217,6 @@ class Config(object):
         # Get role data:
         role_data = self.stack_outputs.get('RoleData', {})
         role_group_vars = self.stack_outputs.get('RoleGroupVars', {})
-        allnodes_vars = self.stack_outputs.get('AllNodesConfig', {})
         role_host_vars = self.stack_outputs.get('AnsibleHostVarsMap', {})
         for role_name, role in six.iteritems(role_data):
             role_path = os.path.join(config_dir, role_name)
@@ -425,12 +424,6 @@ class Config(object):
 
         group_vars_dir = os.path.join(config_dir, 'group_vars')
         self._mkdir(group_vars_dir)
-        # Write allnodes_vars to 'overcloud' group as it doesn't apply
-        # to the undercloud, and 'overcloud' is the inventory group
-        # for all overcloud nodes, regardless of the --stack name.
-        filepath = os.path.join(group_vars_dir, 'overcloud.yaml')
-        with self._open_file(filepath) as vars_file:
-            yaml.safe_dump(allnodes_vars, vars_file, default_flow_style=False)
 
         host_vars_dir = os.path.join(config_dir, 'host_vars')
         self._mkdir(host_vars_dir)
