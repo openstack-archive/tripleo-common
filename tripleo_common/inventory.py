c1af9b7ad94ee46dc280cfeb6e1b0678e70773aa
Oliver Walsh <owalsh@redhat.com>
2020-08-07 16:15:43 +0100

    Ensure tripleo ansible inventory file update is atomic
    Multiple python-tripleoclient commands can run concurrently
    and all attempt to update the same inventory file in
    ~/tripleo-ansible-inventory.yaml. Make the update atomic to
    ensure the inventory file is always complete/valid.
    
    (cherry picked from commit 8e082f45ddfb8f29bd02feb1c9fd1ace4e9ec6dd)
    
    (squashing commits as the 1st patch is failing in the stein gate without
    the fix from the 2nd patch)
    
    Ensure atomic inventory file rename runs on the same mountpoint
    
    Linux rename system call is only supported on the same mountpoint [1].
    /tmp is often a tmpfs mount, so instead generate the temp inventory file in
    the same directory as the target inventory file.
    
    [1] See EXDEV in https://man7.org/linux/man-pages/man2/rename.2.html
    
    Closes-bug: #1892008
    (cherry picked from commit 662b0acd91266d28bf3b0b7a4c68ba919182c56a)
    
    Change-Id: Ifa41bfcb921496978f82aee4e67fdb419cf9ffc5

--- tripleo_common/inventory.py
+++ tripleo_common/inventory.py
@@ -16,8 +16,9 @@
 # under the License.
 
 from collections import OrderedDict
-import os.path
+import os
 import sys
+import tempfile
 import yaml
 
 from heatclient.exc import HTTPNotFound
@@ -356,5 +357,11 @@ class TripleoInventory(object):
                 if var in inventory:
                     inventory[var]['vars'].update(value)
 
-        with open(inventory_file_path, 'w') as inventory_file:
+        # Atomic update as concurrent tripleoclient commands can call this
+        inventory_file_dir = os.path.dirname(inventory_file_path)
+        with tempfile.NamedTemporaryFile(
+                'w',
+                dir=inventory_file_dir,
+                delete=False) as inventory_file:
             yaml.dump(inventory, inventory_file, TemplateDumper)
+        os.rename(inventory_file.name, inventory_file_path)
