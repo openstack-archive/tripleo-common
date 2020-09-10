9e9948e7edc011bcf69b240543f383c7754bb2d8
Luke Short <ekultails@gmail.com>
2020-09-10 15:58:11 -0400

    Adjust Ansible forks caculations
    Using a multiple of 10 can be excessive on resource usage. It has now
    been lowered to 4. The default maximum will also no longer exceed 100.
    
    Change-Id: I57345d5b100efce143fa940b56c81f5e6bc6c390
    Signed-off-by: Luke Short <ekultails@gmail.com>

--- tripleo_common/actions/ansible.py
+++ tripleo_common/actions/ansible.py
@@ -103,7 +103,8 @@ def write_default_ansible_cfg(work_dir,
                     datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
         os.rename(log_path, new_path)
 
-    config.set('defaults', 'forks', str(multiprocessing.cpu_count() * 10))
+    config.set('defaults', 'forks', str(min(
+        multiprocessing.cpu_count() * 4, 100)))
     config.set('defaults', 'timeout', '30')
     config.set('defaults', 'gather_timeout', '30')
 
