20a20ef9e5a0f7d8aeb442538b65b0a6971ec934
Takashi Kajinami <tkajinam@redhat.com>
2021-10-05 20:55:20 +0900

    Remove unused TRIPLEO_NETWORK_CONFIG_RESOURCE
    This constant was added during Train[1] but has never been used
    actually.
    
    [1] 85b7a4eea5c9a36f5a4d2bc4bef5836e2338ab8d
    
    Change-Id: Ib668fbc877139a530d9f491494f8aae027bedace

--- tripleo_common/constants.py
+++ tripleo_common/constants.py
@@ -188,9 +188,6 @@ NESTED_DEPTH = 7
 # Resource name for deployment resources when using config download
 TRIPLEO_DEPLOYMENT_RESOURCE = 'TripleODeployment'
 
-# Resource name for network config resources when using config download
-TRIPLEO_NETWORK_CONFIG_RESOURCE = 'NetworkConfig'
-
 HOST_NETWORK = 'ctlplane'
 
 DEFAULT_VLAN_ID = "1"
