b5ef9a568e7247aeb74ea20832be5d88a24bd124
Cédric Jeanneret <cjeanner@redhat.com>
2022-02-15 10:47:13 +0100

    Ensure failures on the undercloud leads to a complete stop
    With tripleo_free strategy, we might hit situation where the undercloud
    is unreachable for some reasons, preventing external tasks to happen on
    the overcloud nodes.
    
    This is especially true for older releases, such as train, where
    mistral was still used in order to orchestrate the different playbooks
    and runs.
    
    Change-Id: I278fdc9597f83f1dc8390569be9716d3c8847dc4
    Closes-Bug: #1960518
    (cherry picked from commit 80b294ba884ca8ae67137a9885e02edef0b08d4e)
    (cherry picked from commit 5565ecdf68162f7a22f5c570e8d272a39f403826)
    (cherry picked from commit 33f1eecb688855688cd2e88452294ede09590e83)
    (cherry picked from commit 10b421198ebdd53dbc07e03deb258e519723cbd2)

--- tripleo_common/inventory.py
+++ tripleo_common/inventory.py
@@ -170,10 +170,17 @@ class TripleoInventory(object):
                     'ansible_connection': self.undercloud_connection,
                     # see https://github.com/ansible/ansible/issues/41808
                     'ansible_remote_tmp': '/tmp/ansible-${USER}',
+                    # We don't want to let UC fail, especially when it comes to
+                    # external tasks. Thus setting any_error_fatal and
+                    # max_fail_percentage
+                    # See https://bugs.launchpad.net/tripleo/+bug/1960518 for
+                    # context
+                    'any_error_fatal': True,
                     'auth_url': self.auth_url,
                     'cacert': self.cacert,
                     'os_auth_token':
                     self.session.get_token() if self.session else None,
+                    'max_fail_percentage': 0,
                     'plan': None,
                     'project_name': self.project_name,
                     'username': self.username,
