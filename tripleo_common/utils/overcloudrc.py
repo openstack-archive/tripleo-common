055632754f8e8e07b7df10856523117a67de5d15
Takashi Kajinami <tkajinam@redhat.com>
2023-03-28 14:51:49 +0900

    Do not add bracket for IPv6 addresses in no_proxy
    IPv6 addresses in no_proxy environment should NOT be surrounded by [],
    otherwise the addresses are just ignored.
    
    Resolves: rhbz#2180677
    Change-Id: I1b7c9a671078d93ff272d3feef053c1208ab9088
    (cherry picked from commit f88438602528a0e2f07ac7612eab4546077a8780)

--- tripleo_common/utils/overcloudrc.py
+++ tripleo_common/utils/overcloudrc.py
@@ -17,7 +17,6 @@ import logging
 from six.moves import urllib
 
 from tripleo_common import constants
-from tripleo_common.utils import common as common_utils
 
 try:  # py3
     from shlex import quote
@@ -91,10 +90,6 @@ def _create_overcloudrc_from_outputs(
     host = urllib.parse.urlparse(endpoint).hostname
     no_proxy_list = no_proxy.split(',')
     no_proxy_list.extend([host, admin_vip])
-    no_proxy_list = map(
-        common_utils.bracket_ipv6,
-        [i for i in no_proxy_list if i]
-    )
 
     # Remove duplicated entries
     no_proxy_list = sorted(list(set(no_proxy_list)))
